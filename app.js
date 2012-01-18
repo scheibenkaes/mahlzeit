569e44e3d3bd574ac194b5a8c63cc93e51f5815a
Benjamin Klüglein <scheibenkaes@googlemail.com>
2012-01-18 23:04:44 +0100

    Added Brunner button

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
-var url = 'http://helmuts-imbiss.com/index.php?option=com_content&view=article&id=3&Itemid=3';
+var urlHelmut = 'http://helmuts-imbiss.com/index.php?option=com_content&view=article&id=3&Itemid=3';
+var urlBrunner = 'http://www.metzgerei-brunner.org/menu/aktuell.pdf';
 
 function ViewModel () {
     this.menu = ko.observable(null);
+
+    this.onBrunnerMenu = function() {
+        chrome.tabs.create({url: urlBrunner, active: true});
+    }
 }
 
 $(document).ready(function() {
     var viewModel = new ViewModel();
     ko.applyBindings(viewModel);
 
-    $.get(url, function(data){
+    $.get(urlHelmut, function(data){
         var html = $(data);
         var table = $("#page table", html);
         viewModel.menu(table.html());
